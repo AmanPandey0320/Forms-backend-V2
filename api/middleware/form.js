1bc37dbf48f81889da1e453a9654a9fe6b0e3269
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-04-10 19:52:58 +0530

    Update form.js

--- api/middleware/form.js
+++ api/middleware/form.js
@@ -53,4 +53,79 @@ const IS_FORM_TO_USER = async (req,res,next)=>{
     }
 }
 
-module.exports = { IS_FORM_TO_USER };
+const IS_FORM_VALID = async (req,res,next) => {
+    const { form_id } = req.body;
+    const ip = req.connection.remoteAddress;
+    const endpoint = req.originalUrl;
+    const {user_id} = req.user;
+    const info = `submiting forms with form_id : ${form_id} for user with user_id ${user_id}`;
+    let status = '';
+
+    try{
+
+        const sql =`SELECT * FROM form WHERE form_id = ?`;
+        pool.query(sql,[form_id],(err,result)=>{
+            if(err){
+                console.log(err);
+                status = `operation failed with an error : ${JSON.stringify(err)} for user ${user_id}`;
+                logs.add_log(ip,endpoint.info.status);
+                return res.sendStatus(500);
+            }
+
+            if(result === undefined || result.length === 0){
+                status = `no form with id ${form_id} found , by user ${user_id}`;
+                logs.add_log(ip,endpoint,info,status);
+                return res.sendStatus(404);
+            }
+
+            next();
+        })
+
+    }catch(error){
+        console.log(error);
+        status = `operation failed with an error : ${JSON.stringify(error)} for the user ${user_id}`
+        logs.add_log(ip,endpoint,info,status);
+        res.sendStatus(500);
+
+    }
+
+}
+
+const IS_VALID_RESPONSE = async (req,res,next) => {
+
+    const { form_id,response_id } = req.body;
+    const ip = req.connection.remoteAddress;
+    const endpoint = req.originalUrl;
+    const {user_id} = req.user;
+    const info = `fetching response with form_id : ${response_id} for user  ${user_id} form ${form_id}`;
+    let status = '';
+
+    try {
+
+        const sql = `SELECT * FROM response WHERE response_id = ?`;
+        pool.query(sql,[response_id],(err,result)=>{
+            if(err){
+                console.log(err);
+                status = `operation failed with error : ${JSON.stringify(err)}`;
+                logs.add_log(ip,endpoint,info,status);
+                return res.sendStatus(500);
+            }
+            if(result === undefined || result.length === 0){
+                status = `no response found with id : ${response_id}`;
+                logs.add_log(ip,endpoint,info,status);
+                return res.sendStatus(404);
+            }
+
+            next();
+        });
+        
+    } catch (error) {
+        console.log(error);
+        status = `operation failed with error ${JSON.stringify(error)}`;
+        logs.add_log(ip,endpoint,info,status);
+        res.sendStatus(500);
+    }
+
+}
+
+module.exports = { IS_FORM_TO_USER,IS_FORM_VALID,IS_VALID_RESPONSE };
